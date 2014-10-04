e3939fe9d93178a698d7f4c06d599de2786112a4
RemyD <remyd@civx.us>
2014-10-04 16:14:20 -0400

    Added working dev_token things

--- run.py
+++ run.py
@@ -11,6 +11,8 @@ from flask import Flask
 from flask.ext.mako import MakoTemplates, render_template
 from flask import redirect, url_for
 
+from evernote.api.client import EvernoteClient
+
 app = Flask(__name__)
 app.template_folder = "templates"
 mako = MakoTemplates(app)
@@ -20,10 +22,15 @@ config.read('config.ini')
 
 consumer_key = config.get('evernote', 'consumer_key', 0)
 consumer_secret = config.get('evernote', 'consumer_secret', 0)
+dev_token = config.get('evernote', 'dev_token', 0).strip("'")
 
 
 @app.route('/')
 def index():
+    client = EvernoteClient(token=dev_token)
+    userStore = client.get_user_store()
+    user = userStore.getUser()
+    print user.username
     return render_template('index.mak', name='mako', consumer_key=consumer_key,
                            consumer_secret=consumer_secret)
 
