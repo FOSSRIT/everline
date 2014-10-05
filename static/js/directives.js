5f72ca1697a01cc0350430b11a1350d848a8ef7c
Mike Nolan <me@michael-nolan.com>
2014-10-05 00:46:11 -0400

    removed console.log(':punch:')

--- static/js/directives.js
+++ static/js/directives.js
@@ -1,16 +1,13 @@
 "use strict";
 
 var timelineDirectives = angular.module('timelineDirectives', []);
-console.log('directive');
 
 timelineDirectives.directive('timeLine', function () {
-    console.log('fuck');
     return {
         restrict: 'E',
         replace: true,
         template: '<div id="my-timeline"></div>',
         link: function (scope, element, attrs) {
-            console.log('timeline-controller');
             createStoryJS({
                 type:       'timeline',
                 width:      '100%',
